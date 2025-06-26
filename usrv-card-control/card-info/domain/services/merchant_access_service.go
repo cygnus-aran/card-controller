c4c1ad1cf9a0cf989ce04126670e9fc683cf48f5
cygnus-aran <55255091+cygnus-aran@users.noreply.github.com>
2025-06-26 09:07:11 -0400

    Delete usrv-card-control/card-info directory

--- usrv-card-control/card-info/domain/services/merchant_access_service.go
+++
@@ -1,7 +0,0 @@
-package services
-
-// MerchantAccessService defines the interface for checking merchant access
-type MerchantAccessService interface {
-	HasCardInfoAccess(merchantID string) bool
-	IsActiveMerchant(merchantID string) bool
-}
--- usrv-card-control/card-info/domain/services/merchant_key_service.go
+++
@@ -1,7 +0,0 @@
-package services
-
-// MerchantKeyService defines the interface for retrieving merchant public keys
-type MerchantKeyService interface {
-	GetMerchantPublicKey(merchantID string) (string, error)
-	HasMerchantKey(merchantID string) bool
-}
