0f19cf7ba448a06b222d2a1299c567954a6a1e77
cygnus-aran <55255091+cygnus-aran@users.noreply.github.com>
2025-06-26 16:43:53 -0400

    Delete usrv-card-control/features/card-info directory

--- usrv-card-control/features/card-info/domain/services/credential_service.go
+++
@@ -1,6 +0,0 @@
-package services
-
-// CredentialService defines the interface for validating private credentials
-type CredentialService interface {
-	ValidatePrivateCredential(privateCredentialID, merchantID string) bool
-}
